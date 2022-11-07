b4c3bc9978587b788cd76c6c49d7b1d3ad88c3fe
walaaalshaikh <walaalshaikh94@gmail.com>
2022-11-07 19:57:14 +0300

    working on register.dart screen and making the layout

--- lib/view/screens/auth/register.dart
+++ lib/view/screens/auth/register.dart
@@ -1,15 +1,57 @@
+import 'package:ecommerc/utils/theme.dart';
+import 'package:ecommerc/view/widgets/text.dart';
 import 'package:flutter/material.dart';
+
+import '../../widgets/auth/auth_text_field.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(),
-      body: const Center(child: Text("Register Screen",style: TextStyle(
-          fontSize: 30
-      ),),),
-    );
+    return  SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0,
+          ),
+          backgroundColor: Colors.white,
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  width:double.infinity,
+                  height: MediaQuery.of(context).size.height / 1.3,
+                  child:Padding(
+                    padding: EdgeInsets.only(left: 20, right: 20, top: 40),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: const [
+                          CustomText(
+                              text: "SIGN",
+                              fontWeight: FontWeight.w500,
+                              fontSize: 30,
+                              color: mainColor),
+                          SizedBox(width: 5,),
+                          CustomText(
+                              text: "UP",
+                              fontWeight: FontWeight.w500,
+                              fontSize: 30,
+                              color: Colors.black),
+                        ],
+                      ),
+                      const SizedBox(height: 50,),
+                      AuthTextFormField()
+
+
+                    ],
+                  ),),),
+              ],
+            ),
+          ),
+
+
+        ),);
   }
 }
---
+++ lib/view/widgets/auth/auth_text_field.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AuthTextFormField extends StatelessWidget {
+  const AuthTextFormField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+
+    );
+  }
+}
